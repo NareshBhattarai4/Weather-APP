514ebc9cf5cbcf32bbd9f2a3ff880d08d6d794b0
NareshBhattarai4 <110165718+NareshBhattarai4@users.noreply.github.com>
2023-07-08 13:21:28 +0545

    Update script.js

--- script.js
+++ script.js
@@ -18,23 +18,6 @@ const fetchApi = async () => {
     const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${Apikey}`);
     const result = await data.json();
     const dats = result;
-    console.log(dats);
-
-    dats.weather.forEach(e => {
-        if (e.main == "Clouds") {
-            img.src = cloud;
-
-        }else if(e.main == "Clear"){
-            img.src = clear;
-        }else if(e.main == "Drizzle"){
-            img.src = drizzle;
-        }else if(e.main == "Mist"){
-            img.src = mist;
-        }else if(e.main == "Rain"){
-            img.src = rain;
-        }
-    });
-
     if (dats.message) {
         temp.innerText = dats.message;
         cityName.innerText = "";
@@ -49,6 +32,20 @@ const fetchApi = async () => {
 
 
     }
+     dats.weather.forEach(e => {
+        if (e.main == "Clouds") {
+            img.src = cloud;
+
+        }else if(e.main == "Clear"){
+            img.src = clear;
+        }else if(e.main == "Drizzle"){
+            img.src = drizzle;
+        }else if(e.main == "Mist"){
+            img.src = mist;
+        }else if(e.main == "Rain"){
+            img.src = rain;
+        }
+    });
 
 }
 
